f9f4910a17fff2ea55253ab4818f37c802de0f93
saurav <94630933+gjghkvvvhj@users.noreply.github.com>
2023-11-21 17:31:08 +0000

    Update main.js

--- pub/main.js
+++ pub/main.js
@@ -1,5 +1,6 @@
 
-
+const resbtn=document.getElementById('resbtn');
+resbtn.style.display="none";
 const playwaitdiv=document.getElementById('playerwait');
 playwaitdiv.style.display="none";
 const gamediv=document.getElementById('realdgamedivid');
@@ -158,6 +159,50 @@ socket.on('iturncom',data =>{
 
 })
 
+function checkreset(in){
+if(turn>9){
+const resbtn=document.getElementById('resbtn');
+resbtn.style.display="block";
+}
+if(in){
+        const resbtn=document.getElementById('resbtn');
+resbtn.style.display="block";
+}
+}
+
+function reset(){
+c1=document.getElementById('btn1');
+c2=document.getElementById('btn2');
+c3=document.getElementById('btn3');
+c4=document.getElementById('btn4');
+c5=document.getElementById('btn5');
+c6=document.getElementById('btn6');
+c7=document.getElementById('btn7');
+c8=document.getElementById('btn8');
+c9=document.getElementById('btn9');
+        c2.value="";
+         c3.value="";
+         c4.value="";
+         c5.value="";
+         c6.value="";
+         c7.value="";
+         c8.value="";
+         c9.value="";
+         c1.value="";
+
+        c2.innerText="";
+         c3.innerText="";
+         c4.innerText="";
+         c5.innerText="";
+         c6.innerText="";
+         c7.innerText="";
+         c8.innerText="";
+         c9.innerText="";
+         c1.innerText="";
+        turn=1;
+        
+
+}
 function checkwin(){
 c1=document.getElementById('btn1').value;
 c2=document.getElementById('btn2').value;
@@ -170,36 +215,52 @@ c8=document.getElementById('btn8').value;
 c9=document.getElementById('btn9').value;
 
 if(c1+c2+c3=="XXX"){
+        checkreset(true);
 alert('X win the match');
 }else if(c4+c5+c6=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c7+c8+c9=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c1+c4+c7=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c2+c5+c8=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c3+c6+c9=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c7+c5+c3=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c1+c5+c9=="XXX"){
+         checkreset(true);
     alert('X win the match');
 }else if(c1+c2+c3=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c4+c5+c6=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c7+c8+c9=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c1+c4+c7=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c2+c5+c8=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c3+c6+c9=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c7+c5+c3=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }else if(c1+c5+c9=="OOO"){
+         checkreset(true);
     alert('O win the match');
 }
 turn++;
